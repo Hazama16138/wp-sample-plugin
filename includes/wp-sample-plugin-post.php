830c930b9b7d49d1f6a802212a8edd63cac4859f
hazama16138 <like_apple0302@ezweb.ne.jp>
2018-11-20 20:58:47 +0900

    画像アップローダーの実装

--- includes/wp-sample-plugin-post.php
+++ includes/wp-sample-plugin-post.php
@@ -38,15 +38,15 @@
 			$html .= '<tr>';
 			$html .= '<th>画像のURL(必須)</th>';
 			$html .= '<td>';
-			$html .= '<img src="' . plugins_url('../images/no-image.png', __FILE__) . '" width="200" height="">';
-			$html .= '<input type="text" class="large-text" name="sample-image-url">';
-			$html .= '<button class="button">画像を選択</button>';
+			$html .= '<img id="banner-image-view" src="' . plugins_url('../images/no-image.png', __FILE__) . '" width="200" height="">';
+			$html .= '<input id="banner-image-url" type="text" class="large-text" name="sample-image-url">';
+			$html .= '<button id="media-upload" class="button">画像を選択</button>';
 			$html .= '</td>';
 			$html .= '</tr>';
 			
 			$html .= '<tr>';
 			$html .= '<th>画像 ALT属性 (必須)</th>';
-			$html .= '<td><input type="text" class="regular-text" name="sample-image-alt"><p class="description">alt属性のテキストを入力します</p></td>';
+			$html .= '<td><input id="banner-image-alt" type="text" class="regular-text" name="sample-image-alt"><p class="description">alt属性のテキストを入力します</p></td>';
 			$html .= '</tr>';
 			
 			$html .= '<tr>';
@@ -118,5 +118,37 @@
 			$html.= '</div>';
 			
 			echo $html;
+			
+			require_once( plugin_dir_path( __FILE__ ) . 'wp-sample-plugin-upload.php' );
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
