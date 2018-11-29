4d99d6c5f777ed38c985e1df96e78b6d361bd199
hazama16138 <like_apple0302@ezweb.ne.jp>
2018-11-29 21:03:25 +0900

    データベースページの作成

--- includes/wp-sample-admin-db.php
+++ includes/wp-sample-admin-db.php
@@ -56,5 +56,16 @@
 				dbDelta( $query );
 			}
 		}
+		
+		/**
+		* Insert Post.
+		*
+		* @version 1.0.0
+		* @since   1.0.0
+		* @param   array $post
+		*/
+		public function insert_options( array $post ) {
+			var_dump($post);
+		}
 	
 	}
--- wp-sample-plugin.php
+++ wp-sample-plugin.php
@@ -8,6 +8,7 @@ Author: Yoshiki Hazama
 Author URI: https://github.com/Hazama16138/wp-sample-plugin
 License: GPLv2 or later
 */
+require_once( plugin_dir_path( __FILE__ ) . 'includes/wp-sample-admin-db.php' );
 new Sample_Plugin();
 
 class Sample_Plugin {
@@ -40,7 +41,6 @@ class Sample_Plugin {
 	* @since   1.0.0
 	*/
 	public function admin_init() {
-		require_once( plugin_dir_path( __FILE__ ) . 'includes/wp-sample-admin-db.php' );
 		$db = new Sample_Plugin_Admin_Db();
 		$db->create_table(); 
 	}
